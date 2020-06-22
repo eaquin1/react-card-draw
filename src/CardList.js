d26856101e48d2c1c8f081bf658fd65e4e6afc8a
Emily Aquin <eaquin1@gmail.com>
2020-06-22 15:40:21 -0500

    refactored for auto dealing cards

--- src/CardList.js
+++ src/CardList.js
@@ -1,48 +1,76 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
 import axios from "axios";
 
 const CardList = () => {
     const [deckId, setDeckId] = useState(null);
     const [drawn, setDrawn] = useState([]);
+    const [autoDeal, setAutoDeal] = useState(false);
+    const timerRef = useRef(null);
 
+    /* At mount: load deck from API into state */
     useEffect(() => {
         async function loadDeck() {
             const res = await axios.get(
                 `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
             );
             setDeckId(res.data.deck_id);
-            console.log(deckId);
         }
         loadDeck();
     }, []);
 
-    
-        async function drawCard() {
-            const res = await axios.get(
-                `https://deckofcardsapi.com/api/deck/${deckId}/draw`
-            );
-            
-            const card = res.data.cards[0];
-            setDrawn((c) => [
-                ...c,
-                {
-                    id: card.code,
-                    image: card.image,
-                },
-            ]);
-        }
-     
+    /** Draw one card every second if autoDraw is true */
+    useEffect(() => {
+        async function getCard() {
+            try {
+                let drawRes = await axios.get(
+                    `https://deckofcardsapi.com/api/deck/${deckId}/draw`
+                );
 
-    let cards = [];
-    if (drawn !== []) {
-        cards = drawn.map((c) => <Card key={c.id} image={c.image} />);
-    } 
+                if (drawRes.data.remaining === 0) {
+                    setAutoDeal(false);
+                    throw new Error("No cards remaining!");
+                }
+
+                const card = drawRes.data.cards[0];
+
+                setDrawn((c) => [
+                    ...c,
+                    {
+                        id: card.code,
+                        image: card.image,
+                    },
+                ]);
+            } catch (e) {
+                alert(e);
+            }
+        }
+
+        if (autoDeal && !timerRef.current) {
+            timerRef.current = setInterval(async () => {
+                await getCard();
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(timerRef.current);
+            timerRef.current = null;
+        };
+    }, [deckId, autoDeal, setAutoDeal]);
+
+    const toggleDraw = () => {
+        setAutoDeal((deal) => (deal = !deal));
+        console.log(autoDeal);
+    };
+
+    let cards = drawn.map((c) => <Card key={c.id} image={c.image} />);
 
     return (
         <div>
-            <button onClick={drawCard}>Gimme a Card!</button>
-            {cards === [] ? "Draw a Card!" : cards}
+            <button onClick={toggleDraw}>
+                {!autoDeal ? "Turn on AutoDeal" : "Turn off Autodeal"}
+            </button>
+            {cards}
         </div>
     );
 };
